05882ce1231ae6cf5c5395f46c6cb9c587f5b8d7
Tatoh <tatukis@gmail.com>
2024-04-04 15:30:29 -0300

    Fix import paths and add uniqueId field to ITracker interface

--- src/interfaces/tracker.ts
+++ src/interfaces/tracker.ts
@@ -1,5 +1,5 @@
-import { ICliente } from "./cliente";
-import { IColectivo } from "./colectivo";
+import { ICliente } from './cliente';
+import { IColectivo } from './colectivo';
 
 export interface ITraccarDevice {
   // Datos de traccar
@@ -25,6 +25,7 @@ export interface ITracker {
   nombre?: string;
   identificacion?: string;
   asignadoA?: string;
+  uniqueId?: string;
 
   traccar?: ITraccarDevice;
 
@@ -33,10 +34,10 @@ export interface ITracker {
   colectivo?: IColectivo;
 }
 
-type OmitirCreate = "_id" | "cliente";
+type OmitirCreate = '_id' | 'cliente';
 
 export interface ICreateTracker extends Omit<Partial<ITracker>, OmitirCreate> {}
 
-type OmitirUpdate = "_id" | "cliente";
+type OmitirUpdate = '_id' | 'cliente';
 
 export interface IUpdateTracker extends Omit<Partial<ITracker>, OmitirUpdate> {}
